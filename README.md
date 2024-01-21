85c4fcd3e6eef3888811ac985b37948fa929dc11
Rosemberg Porras <rporras@consultec-ti.com>
2024-01-20 22:00:48 -0500

    Restored samconfig configuration file location due to an error that does not allow the config file to be in a different path from the root

--- README.md
+++ README.md
@@ -15,8 +15,5 @@
 Deploy the application to your AWS account using [AWS SAM](https://aws.amazon.com/serverless/sam/).
 
 ```bash
-mvn clean package
-sam deploy -g
+cmd.exe /c deployment\scripts\deploy.bat
 ```
-SAM will create an output of the API Gateway endpoint URL for future use in our load tests.
-Make sure the app name used here matches with the `STACK_NAME` present under `load-test/run-load-test.sh`
--- deployment/scripts/deploy.bat
+++ deployment/scripts/deploy.bat
@@ -1,15 +1,15 @@
 @echo off
 
+cd %~dp0
+cd ../..
+
 echo Initializing params...
 
 SET STACK_NAME=measurement-stack
 SET PARAMS=--resolve-s3 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+SET TEMPLATE_PATH=deployment\template\template.yaml
 
 echo The stack name to be created/updated is %STACK_NAME%...
-
-cd %~dp0
-
-cd ../..
 
 echo Creating JAR file to upload...
 call mvnw package
@@ -20,8 +20,9 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 echo JAR File Created...
 
-cd deployment/scripts
+echo Ruta actual: %CD%
 
-SET TEMPLATE_PATH=../template/template.yaml
+SET FINAL_PARAMS=--template-file %TEMPLATE_PATH% --stack-name %STACK_NAME% %PARAMS%
 
-echo The creation/update of the Stack %STACK_NAME% will start with the next params [%PARAMS%]
+echo The creation/update of the Stack %STACK_NAME% will start with the next params [%FINAL_PARAMS%]
+sam deploy %FINAL_PARAMS%
